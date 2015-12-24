8d8df9d7536eb63ad04462de2e3e1ee6fdf3e059
Marquis Blount <mlblount45@yahoo.com>
2015-12-23 16:30:28 -0800

    adding iterative binary search kata

---
+++ src/com/blountmarquis/algorithms/BinarySearch.java
@@ -0,0 +1,19 @@
+package com.blountmarquis.algorithms;
+
+/**
+ * Created by MLBlount on 12/23/2015.
+ */
+public class BinarySearch {
+
+    public static int findIndex(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
