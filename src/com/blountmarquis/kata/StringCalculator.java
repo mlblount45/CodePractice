0225d30bbb161a37e33f20fae79f5c6be00a285b
Marquis Blount <mlblount45@yahoo.com>
2015-12-29 02:42:21 -0800

    incremental develoment passing multiple numbers test, additional delimeter "\n"

--- src/com/blountmarquis/kata/StringCalculator.java
+++ src/com/blountmarquis/kata/StringCalculator.java
@@ -4,7 +4,19 @@ package com.blountmarquis.kata;
  * Created by MLBlount on 12/29/2015.
  */
 public class StringCalculator {
-    public static int calculate(String s) {
-        return 0;
+    public static int sum(String str) {
+        if (str.length() == 0) return 0;
+        if (str.length() == 1) return Integer.parseInt(str);
+        String[] tokens = str.split("[,\n]+");
+        return getSum(tokens);
+    }
+
+    private static int getSum(String[] tokens) {
+        if (tokens.length < 2) throw new IllegalArgumentException();
+        int sum = 0;
+        for (String s : tokens) {
+            sum += Integer.parseInt(s);
+        }
+        return sum;
     }
 }
--- test/com/blountmarquis/kata/StringCalculatorTest.java
+++ test/com/blountmarquis/kata/StringCalculatorTest.java
@@ -10,6 +10,36 @@ import static org.junit.Assert.assertEquals;
 public class StringCalculatorTest {
     @Test
     public void whenInputStringEmptyThenReturnZero() {
-        assertEquals(0, StringCalculator.calculate(""));
+        assertEquals(0, StringCalculator.sum(""));
+    }
+
+    @Test
+    public void whenInputStingIsSingleIntegerThenReturnInteger() {
+        assertEquals(1, StringCalculator.sum("1"));
+    }
+
+    @Test
+    public void whenInputStringIsOnePlusOneReturnTwo() {
+        assertEquals(2, StringCalculator.sum("1,1"));
+    }
+
+    @Test
+    public void whenInputStringIsOnePlusTwoReturnThree() {
+        assertEquals(3, StringCalculator.sum("1,2"));
+    }
+
+    @Test
+    public void whenInputStringIsOnePlusOnePlusOneThenReturnThree() {
+        assertEquals(3, StringCalculator.sum("1,1,1"));
+    }
+
+    @Test
+    public void whenInputStringContainNewLineDelimiterAndOnePlusOneReturnTwo() {
+        assertEquals(2, StringCalculator.sum("1\n1"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenInputStringContainsMultipleDelimitersAndOnePlusOneNumberThenThrowInvalidFormatException() {
+        assertEquals(2, StringCalculator.sum("1\n"));
     }
 }
