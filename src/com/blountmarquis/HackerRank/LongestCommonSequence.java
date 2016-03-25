66e269d8cb4209cdea3295d55a4d139b9fd65626
Marquis Blount <mlblount45@yahoo.com>
2016-03-24 20:12:17 -0700

    improved read ability more intuitive coding critique

--- src/com/blountmarquis/HackerRank/LongestCommonSequence.java
+++ src/com/blountmarquis/HackerRank/LongestCommonSequence.java
@@ -26,7 +26,7 @@ public class LongestCommonSequence {
         for (int i = 1; i <= s1.length; i++) {
             for (int j = 1; j <= s2.length; j++) {
                 if (s1[i-1] == s2[j-1]) {
-                    memo[i][j] = 1 + memo[i - 1][j - 1];
+                    memo[i][j] = memo[i - 1][j - 1] + 1;
                 } else memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
             }
         }
